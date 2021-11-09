deffca014c815ecdd2a0982059b4b1af47edc6b2
SteRap <92691510+SteRap@users.noreply.github.com>
2021-11-09 17:45:43 +0100

    several fixes

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const signin = require("./Controllers/signin");
 const profile = require("./Controllers/profile");
 const image = require("./Controllers/image");
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
   client: "pg",
-  connection: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const app = express();
